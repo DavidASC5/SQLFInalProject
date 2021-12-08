9f73ac648b53b4b86b8d74a3a440f6e9f19b565f
David Maduako <achooospace@gmail.com>
2021-12-08 00:56:13 -0500

    step 12
    fsfef

--- SQL Project_DMaduako.py
+++ SQL Project_DMaduako.py
@@ -3,7 +3,8 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="Mysterious01"
+  password="Mysterious01",
+  database="menagerie"
 )
 
 print(mydb)
@@ -11,8 +12,12 @@ print(mydb)
 mycursor = mydb.cursor()
 
 #mycursor.execute("CREATE DATABASE menagerie")
-mycursor.execute("DROP DATABASE menagerie")
-mycursor.execute("SHOW DATABASES")
+#mycursor.execute("DROP DATABASE menagerie")
+mycursor.execute("CREATE TABLE pets (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex VARCHAR(1), birth DATE, death DATE)")
+mycursor.execute("SHOW TABLES")
 
+for x in mycursor:
+  print(x)
+mycursor.execute("DESCRIBE pets")
 for x in mycursor:
   print(x)
